a02d6dc410ba44402d9bee3e2ef4bfc2c91c04fe
username <filipioan_29@yahoo.ro>
2026-02-21 20:28:25 +0200

    Added Axios interceptor for CSRF token

--- ecom-frontend/src/api/api.js
+++ ecom-frontend/src/api/api.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:`${import.meta.env.VITE_BACK_END_URL}/api`,
-    withCredentials: true, // 🔥 OBLIGATORIU pentru cookies
+    baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,
+    withCredentials: true,
 });
 
-
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("JWT_TOKEN");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+    const csrfToken = document.cookie
+        .split("; ")
+        .find(row => row.startsWith("XSRF-TOKEN="))
+        ?.split("=")[1];
+
+    if (csrfToken) {
+        config.headers["X-XSRF-TOKEN"] = csrfToken;
     }
+
     return config;
 });
 
