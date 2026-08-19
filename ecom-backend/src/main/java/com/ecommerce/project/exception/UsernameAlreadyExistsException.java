006726a2a43137100af3304d357ac8fd75a7ba92
username <filipioan_29@yahoo.ro>
2026-08-19 09:26:17 +0300

    refactor(auth): typed exceptions in AuthService and centralized handlers

---
+++ ecom-backend/src/main/java/com/ecommerce/project/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.ecommerce.project.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
