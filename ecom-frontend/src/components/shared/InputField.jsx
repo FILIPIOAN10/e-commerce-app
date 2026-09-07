38dbc2c068174a2578b50dafe89e99e2e7b924e5
Filip <filipioan_29@yahoo.ro>
2026-09-07 11:17:37 +0300

    fix: adaugat clase de dark mode pentru InputField

--- ecom-frontend/src/components/shared/InputField.jsx
+++ ecom-frontend/src/components/shared/InputField.jsx
@@ -45,11 +45,11 @@ const InputField = ({
     return  (
         <div className="flex flex-col gap-1 w-full">
             <label
-            
-            htmlFor={id}
-            className={`${
-                className ? className : ""
-            } font-semibold text-sm text-slate-800 dark:text-gray-200}`}>
+                htmlFor={id}
+                className={`${
+                    className ? className : ""
+                } font-semibold text-sm text-slate-800 dark:text-gray-200`}
+            >
                 {label}
             </label>
             <input
@@ -63,7 +63,7 @@ const InputField = ({
             placeholder={placeHolder}
             className={`${
                 className ? className : ""
-            } px-2 py-2 border outline-none ${readOnly ? "bg-slate-100":"bg-transparent"} text-slate-800 dark:text-white dark:bg-gray-700 dark:border-gray-600 rounded-md ${
+            } px-2 py-2 border outline-none ${readOnly ? "bg-slate-100":"bg-transparent"} text-slate-800 dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 rounded-md ${
                 errors[id]?.message ? "border-red-500": "border-slate-700 dark:border-gray-600"
             }`}
 
